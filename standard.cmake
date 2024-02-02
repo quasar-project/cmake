da4aee28248a7d54417e463335d0ee0094803c14
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-02-02 19:20:22 +0300

    Create standard.cmake

---
+++ standard.cmake
@@ -0,0 +1,15 @@
+# Correctly set C++ standard.
+# Bonus feature: fixes Visual Studio generator =)
+# Usage: set_language_standard(20)
+macro(set_language_standard __STANDARD)
+  if(NOT CMAKE_CXX_STANDARD)
+    message(STATUS "[${PROJECT_NAME}] setting c++ standard to ${__STANDARD}")
+    set(CMAKE_CXX_STANDARD ${__STANDARD})
+    set(CMAKE_CXX_STANDARD_REQUIRED ON)
+    set(CMAKE_CXX_EXTENSIONS OFF)
+  endif(NOT CMAKE_CXX_STANDARD)
+
+  if("${CMAKE_GENERATOR}" MATCHES "^Visual Studio")
+    set(CMAKE_GENERATOR_PLATFORM "x64" CACHE STRING "" FORCE)
+  endif()
+endmacro()
