d092f5573af7fc72f3fba08701bb24301df896cf
whs31 <75864292+whs31@users.noreply.github.com>
2024-02-20 21:37:46 +0300

    Update metadata.cmake

--- metadata.cmake
+++ metadata.cmake
@@ -1,12 +1,15 @@
 # Set metadata compile definitions for target.
 # Currently sets PROJECT_NAME, PROJECT_VERSION, PROJECT_HOMEPAGE_URL, PROJECT_COMPANY defs.
 # Usage: target_add_metadata_definitions(<target>)
-macro(target_add_metadata_definitions __TARGET)
+function(target_add_metadata_definitions __TARGET)
+  set(company COMPANY)
+  cmake_parse_arguments(TARGET_ADD_INTEGRATION_TESTS "" "${company}" "" ${ARGN})
+  message(STATUS "[${PROJECT_NAME}] setting company: ${TARGET_ADD_INTEGRATION_TESTS_COMPANY}")
   target_compile_definitions(${__TARGET}
     PRIVATE
       -DPROJECT_NAME="${PROJECT_NAME}"
       -DPROJECT_VERSION="${PROJECT_VERSION}"
-      -DPROJECT_COMPANY="Radar MMS"
+      -DPROJECT_COMPANY=${TARGET_ADD_INTEGRATION_TESTS_COMPANY}
       -DPROJECT_DOMAIN="${PROJECT_HOMEPAGE_URL}"
   )
-endmacro()
+endfunction()
